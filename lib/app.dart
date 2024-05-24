bfbc88b02261dc06218f9e69faa37488847d7620
raman04-byte <ramantank04022002@gmail.com>
2024-05-24 22:39:21 +0530

    Build the app architecture and following MVVM arc.

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: const [],
+      child: const MaterialApp(
+        title: 'Resume',
+      ),
+    );
+  }
+}
