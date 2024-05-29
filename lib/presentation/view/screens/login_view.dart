9ae807cf8cf70ecfacb01a2b07e68d70fad40f47
raman04-byte <ramantank04022002@gmail.com>
2024-05-29 23:14:08 +0530

    Updation in login view

--- lib/presentation/view/screens/login_view.dart
+++ lib/presentation/view/screens/login_view.dart
@@ -37,7 +37,7 @@ class _LoginViewState extends State<LoginView> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: Text(resources.AppStrings().login),
         actions: [
           IconButton(
             onPressed: () {
